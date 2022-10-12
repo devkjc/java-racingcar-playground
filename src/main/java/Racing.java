4649e9cca07a3a4a30981b0d9058d67c5ba05a8d
JongChan <kjc3590@songareeit.com>
2022-10-12 09:52:16 +0900

    feat: winnerTest

--- src/main/java/Racing.java
+++ src/main/java/Racing.java
@@ -4,34 +4,48 @@ import java.util.stream.Collectors;
 
 public class Racing {
 
-    private final List<Car> carList = new ArrayList<>();
+    private final List<Car> carList;
 
     public Racing(String names) {
+
+        carList = new ArrayList<>();
+
         String[] split = names.split(",");
         for (String name : split) {
             carList.add(new Car(name));
         }
     }
 
+    public Racing(List<Car> carList) {
+        this.carList = carList;
+    }
+
     public void goCars(int repeat) throws InterruptedException {
         System.out.println("실행결과");
         System.out.println("========================================= :: ");
         for (int i = 1; i <= repeat; i++) {
-            roundStart(i);
+            System.out.println(i + " 라운드");
+            roundStart();
         }
-        winner();
+        winnerPrint();
         System.out.println("========================================= :: ");
     }
 
-    private void winner() {
-        int winnerPoint = carList.stream().mapToInt(Car::getPoint).max().orElse(0);
-        String winnerName = carList.stream().filter(car -> car.getPoint() == winnerPoint).map(Car::getName).collect(Collectors.joining(","));
-        System.out.println("우승자는 :: " + winnerName);
+    private void winnerPrint() {
+        String winner = getWinner(getWinnerPoint()).stream().map(Car::getName).collect(Collectors.joining(","));
+        System.out.println("우승자는 :: " + winner);
     }
 
-    private void roundStart(int i) throws InterruptedException {
+    public List<Car> getWinner(int winnerPoint) {
+        return carList.stream().filter(car -> car.getPoint() == winnerPoint).collect(Collectors.toList());
+    }
+
+    public int getWinnerPoint() {
+        return carList.stream().mapToInt(Car::getPoint).max().orElse(0);
+    }
+
+    private void roundStart() throws InterruptedException {
         Thread.sleep(1000);
-        System.out.println(i + " 라운드");
         for (Car car : carList) {
             car.go();
             System.out.println(car);
--- src/test/java/RacingTest.java
+++ src/test/java/RacingTest.java
@@ -1,21 +1,32 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RacingTest {
 
-    Racing racing;
-
-    @BeforeEach
-    void setUp() {
-        racing = new Racing("이름이지롱,이름이지지,이름이다,이름,아하");
+    @Test
+    void 차_여러대_생성() {
+        Racing racing = new Racing("이름이지롱,이름이지지,이름이다,이름,아하");
+        assertThat(racing).isInstanceOf(Racing.class);
+        assertThatThrownBy(() -> new Racing("이름인데길어")).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void 차_여러대_생성() {
-        assertThat(racing).isInstanceOf(Racing.class);
-        assertThatThrownBy(() -> new Racing("이름인데길어")).isInstanceOf(IllegalArgumentException.class);
+    void 우승자() {
+
+        Car car1 = new Car("1");
+        car1.go(4);
+        car1.go(4);
+        Car car2 = new Car("2");
+        car2.go(4);
+        car2.go(4);
+        Car car3 = new Car("3");
+
+        Racing racing = new Racing(Arrays.asList(car1,car2,car3));
+
+        assertThat(racing.getWinner(racing.getWinnerPoint())).contains(car1, car2);
     }
 }
